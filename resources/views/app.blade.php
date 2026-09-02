0093da0bf85cdb9ebb679eb1e04a85b15057310f
Patrickevra254 <patrickevra254@gmail.com>
2026-09-02 13:08:54 +0100

    feat: port GCFitness design tokens, utilities, and font loading

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,15 +5,30 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bebas+Neue&family=Oswald:wght@300;400;500;600;700&family=Inter:wght@300;400;500;600;700;800&display=swap">
+
+    <script>
+        (function () {
+            try {
+                var t = localStorage.getItem('gcfitness-theme');
+                var d = t || (window.matchMedia('(prefers-color-scheme: light)').matches ? 'light' : 'dark');
+                document.documentElement.classList.add(d);
+            } catch (e) {
+                document.documentElement.classList.add('dark');
+            }
+        })();
+    </script>
+
     @viteReactRefresh
-    @vite('resources/js/app.jsx')
+    @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @inertiaHead
-    @routes
 </head>
 
 <body>
     @inertia
-
 </body>
 
 </html>
